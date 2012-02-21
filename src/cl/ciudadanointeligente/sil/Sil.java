bd9c4912c512ba05740e66d568b6ce02dc3b1067
Juan Pablo Perez T <jpperez@ciudadanointeligente.cl>
2012-02-21 12:29:34 -0300

    Sil: Normalizes code

--- src/cl/ciudadanointeligente/sil/Sil.java
+++ src/cl/ciudadanointeligente/sil/Sil.java
@@ -1,20 +1,20 @@
 package cl.ciudadanointeligente.sil;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.hibernate.Session;
-
 import cl.ciudadanointeligente.sil.model.SilBill;
 import cl.ciudadanointeligente.sil.model.SilSession;
 import cl.ciudadanointeligente.sil.parser.BillParser;
 import cl.ciudadanointeligente.sil.parser.DiputeeChamberSessionParser;
 import cl.ciudadanointeligente.sil.processor.BillProcessor;
 import cl.ciudadanointeligente.sil.processor.MergedBillProcessor;
+
+import org.hibernate.Session;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Sil {
 	static final int MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
@@ -28,44 +28,45 @@ public class Sil {
 	static Session session;
 
 	public static void main(String[] args) throws Throwable {
-		boolean test = isArgumentPresent(args, "-test");
-		if (test)
+		boolean testMode = isArgumentPresent(args, "-test");
+		if (testMode) {
 			System.out.println("MODO TEST - no se guardará nada en la base de datos");
+		}
 
 		session = HibernateUtil.getSession();
 		df = new SimpleDateFormat("dd/MM/yyyy");
 		billParser = new BillParser();
 		diputeeChamberSessionParser = new DiputeeChamberSessionParser();
-		billProcessor = new BillProcessor(df, test);
-		mergedBillProcessor = new MergedBillProcessor(billProcessor,
-				billParser, test);
+		billProcessor = new BillProcessor(df, testMode);
+		mergedBillProcessor = new MergedBillProcessor(billProcessor, billParser, testMode);
 
 		// PROCESSING ARGUMENT
 		String bulletinNumber = getArgument(args, "-bulletin");
-
 		String startDateString = getArgument(args, "-startDate");
 		String endDateString = getArgument(args, "-endDate");
 
 		Date startDate = toDate(startDateString, df);
 		Date endDate = toDate(endDateString, df);
-		if (startDate == null)
-			startDate = df.parse(df.format(new Date()));
-		if (endDate == null)
-			endDate = getNextDay(startDate);
 
+		if (startDate == null) {
+			startDate = df.parse(df.format(new Date()));
+		}
+		if (endDate == null) {
+			endDate = getNextDay(startDate);
+		}
 		if (bulletinNumber != null) {
 			System.out.println("Procesando boletin: " + bulletinNumber);
 			processBulletin(bulletinNumber);
 		}
-		System.out.println("Procesando: " + df.format(startDate) + " - "
-				+ df.format(endDate));
+		System.out.println("Procesando: " + df.format(startDate) + " - " + df.format(endDate));
 		processTimeSpan(startDate, endDate);
-
 	}
-	private static Date getNextDay(Date date){
+
+	private static Date getNextDay(Date date) {
 		long time = date.getTime() + MILLIS_IN_DAY;
 		return new Date(time);
 	}
+
 	public static Date toDate(String date, DateFormat df) {
 		try {
 			return df.parse(date);
@@ -78,20 +79,24 @@ public class Sil {
 	}
 
 	public static boolean isArgumentPresent(String args[], String arg) {
-		for (String eachArg : args)
-			if (eachArg.equals(arg))
+		for (String eachArg : args) {
+			if (eachArg.equals(arg)) {
 				return true;
+			}
+		}
 		return false;
 	}
 
 	public static String getArgument(String args[], String arg) {
 		try {
-			for (int i = 0; i < args.length; i++)
-				if (args[i].equals(arg))
+			for (int i = 0; i < args.length; i++) {
+				if (args[i].equals(arg)) {
 					return args[i + 1];
-			return null;// not found
+				}
+			}
+			return null; // not found
 		} catch (IndexOutOfBoundsException e) {
-			return null;// argument name was present but there was no value
+			return null; // argument name was present but there was no value
 		}
 	}
 
@@ -112,28 +117,29 @@ public class Sil {
 		}
 	}
 
-	public static void processTimeSpan(Date startDate, Date endDate)
-			throws Throwable {
+	public static void processTimeSpan(Date startDate, Date endDate) throws Throwable {
 		processTimeSpanBill(startDate, endDate);
 		processTimeSpanSession(startDate, endDate);
 	}
-	public static void processTimeSpanSession(Date startDate, Date endDate)throws Throwable{
+
+	public static void processTimeSpanSession(Date startDate, Date endDate) throws Throwable {
 		try {
-			//session.beginTransaction();
+			// session.beginTransaction();
 			int[][] legislatures = diputeeChamberSessionParser.findLegislatures();
 			List<SilSession> sessions = new ArrayList<SilSession>();
-			for(int[] legislature: legislatures){
+			for (int[] legislature : legislatures) {
 				List<SilSession> sessionsTmp = diputeeChamberSessionParser.getSessionSummaryFromLegislature(legislature[0], startDate, endDate);
 				sessions.addAll(sessionsTmp);
 			}
-			//session.getTransaction().commit();
+			// session.getTransaction().commit();
 		} catch (Throwable ex) {
 			ex.printStackTrace(System.err);
-			//session.getTransaction().rollback();
+			// session.getTransaction().rollback();
 			throw ex;
 		}
 	}
-	public static void processTimeSpanBill(Date startDate, Date endDate)throws Throwable{
+
+	public static void processTimeSpanBill(Date startDate, Date endDate) throws Throwable {
 		try {
 			session.beginTransaction();
 			for (SilBill newBill : billParser.getBills(startDate, endDate)) {
